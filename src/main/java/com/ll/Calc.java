b354fc55f69297122b39e95305d1a3ac5f185c91
jaeuklim <jeuk007@skuniv.ac.kr>
2023-11-06 15:24:35 +0900

    더하기 구현

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -2,9 +2,17 @@ package com.ll;
 
 public class Calc {
     public static int run(String exp) {
-        if(exp.contains("-"))
+        if (exp.isBlank())
             return 0;
 
-        return 20;
+        if (exp.contains("-"))
+            return 0;
+
+        String[] exps = exp.split(" \\+ ");
+
+        int num1 = Integer.parseInt(exps[0]);
+        int num2 = Integer.parseInt(exps[1]);
+
+        return num1 + num2;
     }
 }
